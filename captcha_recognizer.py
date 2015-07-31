017c759c9935f496c03e026be22d4bb5da2d84b6
LiZifan <zifanli@umich.edu>
2015-07-30 21:10:09 -0400

    c

--- captcha_recognizer.py
+++ captcha_recognizer.py
@@ -1,9 +1,15 @@
-# from PIL import Image
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
 
 # TODO
+
 
 class CaptchaRecognizer:
     def recognize(self, img):
         plt.clf()
-        plt.hist(rgb_to_hsv(img)[:, :, 0].flatten(), bins=512, range=(0, 1))
+        plt.imshow(img)
+        plt.show()
+        plt.hist(colors.rgb_to_hsv(img)[:, :, 0].flatten(), bins=512, range=(0, 1))
         plt.savefig('temp/00.origin.hue.hist.png')
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # Currently for testing
 
 import dataset_manager
+from captcha_recognizer import CaptchaRecognizer
+import captcha_source
 
 
 def main():
-    test_fetch_training_set()
+    # test_fetch_training_set()
+    test_captcha_recognizing()
 
 
 def test_fetch_training_set():
     dataset_manager.clear_training_set()
     dataset_manager.fetch_training_set(5)
+
+
+def test_captcha_recognizing():
+    image = captcha_source.fetch_image()
+    CaptchaRecognizer().recognize(image)
 
 
 if __name__ == '__main__':
