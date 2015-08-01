f229a119718031b6b9b226dffad716fbd244026f
Zhang NS <zhanglinghan120@gmail.com>
2015-08-01 23:50:26 +0800

    Also use S and V when removing noise

--- captcha_recognizer.py
+++ captcha_recognizer.py
@@ -5,61 +5,83 @@ import matplotlib.image as mpimg
 import numpy as np
 import config as c
 
+
+# E.g. _sort_by_occurrence(np.array([1, 3, 3, 1, 2, 2, 2, 3, 4, 2]))
+# Return: array([2, 3, 1, 4])
+def _sort_by_occurrence(arr):
+    arr = np.sort(arr)
+    val_list = [-1]
+    occur_list = [0]
+    for val in arr:
+        if val == val_list[-1]:
+            occur_list[-1] += 1
+        else:
+            val_list.append(val)
+            occur_list.append(1)
+    occur_array = np.array(occur_list)
+    sort_index = occur_array.argsort()[:0:-1]
+    val_array = np.array(val_list)
+    return val_array[sort_index]
+
+
+def _rgb_to_int(rgb):
+    return int(colors.rgb2hex(rgb)[1:], 16)
+
+
+def _int_to_rgb(n):
+    return colors.hex2color('#{:06x}'.format(n))
+
+
 # Color map for binary images
-cm = plt.cm.get_cmap('Greys')
+_cm = plt.cm.get_cmap('Greys')
 
 
 # Show binary image in matplotlib window
-def show_image(img):
+def _show_image(img):
     plt.clf()
     plt.axis('off')
-    plt.imshow(img, cmap=cm)
+    plt.imshow(img, cmap=_cm)
     plt.show()
 
 
 class CaptchaRecognizer:
     def __init__(self):
-        self.hue_tolerance = 0.012
-        self.neighbor_low = 1
+        self.h_tolerance = 5 / 360
+        self.s_tolerance = 30 / 100
+        self.v_tolerance = 40 / 100
+        self.neighbor_low = 0
         self.neighbor_high = 5
         self.sep_constant = 0.016
 
     def recognize(self, img):
         mpimg.imsave(c.temp_path('00.origin.png'), img)
 
-        img_hue = self.remove_noise_with_hue(img)
-        mpimg.imsave(c.temp_path('01.hue.png'), img_hue, cmap=cm)
+        img_01 = self.remove_noise_with_hsv(img)
+        mpimg.imsave(c.temp_path('01.hsv.png'), img_01, cmap=_cm)
 
-        img_neighbor = self.remove_noise_with_neighbors(img_hue)
-        mpimg.imsave(c.temp_path('02.neighbor.png'), img_neighbor, cmap=cm)
+        img_02 = self.remove_noise_with_neighbors(img_01)
+        mpimg.imsave(c.temp_path('02.neighbor.png'), img_02, cmap=_cm)
 
-        img_seperate = self.find_vertical_separation_line(img_neighbor)
-        mpimg.imsave(c.temp_path('03.seperate.png'), img_seperate, cmap=cm)
+        img_03 = self.find_vertical_separation_line(img_02)
+        mpimg.imsave(c.temp_path('03.separate.png'), img_03, cmap=_cm)
 
-    def remove_noise_with_hue(self, img):
-        img_hsv = colors.rgb_to_hsv(img)
-        hue_array = img_hsv[:, :, 0].flatten()
-        hue_array.sort()
-        hue_list = [-1]
-        occur_list = [0]
-        max_occurs = 1
-        for hue in hue_array:
-            if hue == hue_list[-1]:
-                occur_list[-1] += 1
-            else:
-                if occur_list[-1] > max_occurs:
-                    max_occurs = occur_list[-1]
-                hue_list.append(hue)
-                occur_list.append(1)
-        occur_array = np.array(occur_list)
-        sort_index = occur_array.argsort()[::-1]
-        std_h = hue_list[sort_index[1]]
+    def remove_noise_with_hsv(self, img):
+        # Use number of occurrences to find the standard h, s, v
+        # Convert to int so we can sort the colors
+        img_int = np.apply_along_axis(_rgb_to_int, 2, img)
+        color_array = _sort_by_occurrence(img_int.flatten())
+        std_color = color_array[1]
+        std_h, std_s, std_v = colors.rgb_to_hsv(_int_to_rgb(std_color))
+        # print(std_h * 360, std_s * 100, std_v * 100)
         Y, X, _ = img.shape
         new_img = np.zeros((Y, X))
+        img_hsv = colors.rgb_to_hsv(img)
         for y in range(Y):
             for x in range(X):
                 h, s, v = img_hsv[y, x, :]
-                if abs(h - std_h) <= self.hue_tolerance:
+                if abs(h - std_h) <= self.h_tolerance and \
+                                abs(s - std_s) <= self.s_tolerance and \
+                                abs(v - std_v) <= self.v_tolerance:
                     new_img[y, x] = 1
         return new_img
 
