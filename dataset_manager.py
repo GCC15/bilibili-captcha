fd824c224b77d13ef2ac28290eae2d1a8b873ccb
LiZifan <zifanli@umich.edu>
2015-08-02 17:52:28 -0400

    converting

--- dataset_manager.py
+++ dataset_manager.py
@@ -7,6 +7,7 @@ import matplotlib.image as mpimg
 import captcha_source
 import config as c
 from captcha_recognizer import CaptchaRecognizer
+import time
 
 _cm_greys = plt.cm.get_cmap('Greys')
 _png = '.png'
@@ -30,7 +31,8 @@ def _fetch_dir(directory, num=1, use_https=False):
         if len(seq) != captcha_source.captcha_length:
             raise ValueError('Incorrect length')
         # Assume the char sequence is not met before
-        mpimg.imsave(os.path.join(directory, _get_filename_from_basename(seq)), img)
+        mpimg.imsave(os.path.join(directory, _get_filename_from_basename(seq)),
+                     img)
     plt.ioff()
 
 
@@ -114,40 +116,66 @@ def _list_png(directory):
     return list(filter(png_filter, os.listdir(directory)))
 
 
+def _list_unconverted_png(directory):
+    list = _list_png(directory)
+    final_list = []
+    for i in range(len(list)):
+        if list[i][5:9] != ".con":
+            final_list.append(list[i])
+    return final_list
+
+
 def _list_seq(directory):
     return list(map(_get_basename_from_filename, _list_png(directory)))
 
 
+def _list_unconverted_seq(directory):
+    return list(
+        map(_get_basename_from_filename, _list_unconverted_png(directory)))
+
+
+def _convert_all_training_png_to_original_name():
+    png_list = _list_png(c.training_set_dir)
+    for i in range(len(png_list)):
+        os.rename(os.path.join(c.training_set_dir, png_list[i]),
+                  os.path.join(c.training_set_dir, (png_list[i][0:5] + '.png')))
+
+
 def convert_training_image_to_char(force_update=False):
-    num_total = 0
+    start = time.time()
     num_update = 0
     num_success = 0
     recognizer = CaptchaRecognizer()
-    seq_list = _list_seq(c.training_set_dir)
+    if force_update:
+        _convert_all_training_png_to_original_name()
+    num_total = len(_list_png(c.training_set_dir))
+    seq_list = _list_unconverted_seq(c.training_set_dir)
+    file_list = _list_unconverted_png(c.training_set_dir)
     for s in range(len(seq_list)):
-        num_total += 1
         seq = seq_list[s]
         print('{}/{}: {}'.format(s, len(seq_list), seq))
-        if force_update or not os.path.isfile(
-                c.char_path(seq[0],
-                _get_filename_from_basename('{}.{}'.format(seq, 1)))):
-            num_update += 1
-            img = get_training_image(seq)
-            img_01 = recognizer.remove_noise_with_hsv(img)
-            img_02 = recognizer.remove_noise_with_neighbors(img_01)
-            img_02 = recognizer.remove_noise_with_neighbors(img_02)
-            _, cut_line = recognizer.find_vertical_separation_line(img_02)
-            img_list = recognizer.cut_images(img_02, cut_line)
-            if len(img_list) == captcha_source.captcha_length:
-                num_success += 1
-                for i in range(captcha_source.captcha_length):
-                    path = c.char_path(
-                        seq[i],
-                        _get_filename_from_basename('{}.{}'.format(seq, i + 1)))
-                    mpimg.imsave(path, img_list[i], cmap=_cm_greys)
+        num_update += 1
+        img = get_training_image(seq)
+        img_01 = recognizer.remove_noise_with_hsv(img)
+        img_02 = recognizer.remove_noise_with_neighbors(img_01)
+        img_02 = recognizer.remove_noise_with_neighbors(img_02)
+        _, cut_line = recognizer.find_vertical_separation_line(img_02)
+        img_list = recognizer.cut_images(img_02, cut_line)
+        if len(img_list) == captcha_source.captcha_length:
+            num_success += 1
+            for i in range(captcha_source.captcha_length):
+                path = c.char_path(
+                    seq[i],
+                    _get_filename_from_basename('{}.{}'.format(seq, i + 1)))
+                mpimg.imsave(path, img_list[i], cmap=_cm_greys)
+        os.rename(os.path.join(c.training_set_dir, file_list[s]),
+                  os.path.join(c.training_set_dir,
+                               (file_list[s][0:-4] + '.con.png')))
     print('Total: {}'.format(num_total))
     print('Update: {}'.format(num_update))
     print('Success: {}'.format(num_success))
+    end = time.time()
+    print('Used time:', end - start)
 
 
 if __name__ == '__main__':
