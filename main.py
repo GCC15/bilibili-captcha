7ebfec61a11376e10a106207b26982f32945d258
LiZifan <zifanli@umich.edu>
2015-08-13 18:48:30 +0800

    +40 data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from captcha_recognizer import CaptchaRecognizer
 
 def main():
     # dataset_manager.fetch_training_set(100)
-    test_captcha_recognition()
-    # dataset_manager.partition_training_images_to_chars()
+    # test_captcha_recognition()
+    dataset_manager.partition_training_images_to_chars()
     # dataset_manager.partition_training_images_to_chars(force_update=True, save=True)
     # dataset_manager.tune_partition_parameter()
 
